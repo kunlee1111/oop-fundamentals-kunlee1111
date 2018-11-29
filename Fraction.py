91ed7aa0b5ee653ee418695eda0b7d06e8382d16
robuntu <robuntu@stro.ycdsb.ca>
2018-11-29 10:09:02 -0500

    committing class Fraction and initialization method

--- Fraction.py
+++ Fraction.py
@@ -3,17 +3,7 @@ class Fraction(object):
         self.numerator = numerator
         self.denominator = denominator
         if self.denominator == 0:
-            ValueError("denominator can't be zero")
-
-    def __str__(self):
-        return str(self.numerator) + "/" + str(self.denominator)
-    def show(self):
-        print(self.numerator,"/",self.denominator)
-    def __add__(self, other_Fraction):
-        new_Numerator = self.numerator*other_Fraction.denominator + self.denominator*other_Fraction.numerator
-        new_Denominator = self.denominator*other_Fraction.denominator
-        common = gcd(new_Denominator, new_Denominator)
-        
+            raise ValueError("denominator can't be zero")
 
 if __name__ == '__main__':
   # put your code that utilizes your Fraction class here
