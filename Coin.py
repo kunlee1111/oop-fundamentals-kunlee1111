2e4316bf69e885e874baa0a9c46e907148c9a6aa
Kun Lee <kun.lee19@ycdsbk12.ca>
2018-11-28 23:18:12 -0500

    committing the coin class usage.

--- Coin.py
+++ Coin.py
@@ -7,9 +7,20 @@ class Coin():
         return self.face
 
     def flip(self):
-        return str(random.choice(["heads", "tails"]))
+        return str(random.choice(["Heads", "Tails"]))
 
 if __name__ == '__main__':
-  # put your code that utilizes your Coin class here
-  pass
+    a = 0
+    total_Heads = 0
+    total_Tails = 0
+    coin = Coin()
+    while a <= 1000:
+        coin.flip()
+        a += 1
+        if coin.get_face() == 'Heads':
+            total_Heads += 1
+        else:
+            total_Tails += 1
+    print ("Total Heads Count is"+ total_Heads + "Total Tails Count is"+ total_Tails)
 
+    pass
