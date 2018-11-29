121e802a9612ad1661349c3a4a8fce598d371f00
Kun Lee <kun.lee19@ycdsbk12.ca>
2018-11-28 23:02:46 -0500

    committing the function get_face

--- Coin.py
+++ Coin.py
@@ -1,12 +1,17 @@
+from random import randint
 class Coin():
     def __init__(self):
         self.face = 'Heads'
 
     def get_face(self):
         return self.face
-    
 
-
+    def flip(self):
+        temporary_Variable = randint(0,1)
+        if temporary_Variable == 1:
+            self.face = "Heads"
+        else:
+            self.face = "Tails"
 
 if __name__ == '__main__':
   # put your code that utilizes your Coin class here
