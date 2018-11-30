ab57a104da86c715fee21e54946042f331abe32c
robuntu <robuntu@stro.ycdsb.ca>
2018-11-30 10:15:29 -0500

    commiting changes to codes

--- Fraction.py
+++ Fraction.py
@@ -1,6 +1,5 @@
-
 class Fraction(object):
-    def __int__(self, numerator, denominator):
+    def __init__(self, numerator, denominator):
         self.numerator = numerator
         self.denominator = denominator
         if self.denominator == 0:
@@ -21,29 +20,44 @@ class Fraction(object):
             raise ValueError("denominator can't be zero")
 
     def __str__(self):
-        return str(self.numerator)+"/"+str(self.denominator)
+        return "{}/{}".format(int(self.numerator), int(self.denominator))
 
     def __reduce(self):
         gcd = lambda m, n: m if not n else gcd(n, m % n)
+        # lcm = lambda m, n: m * n / gcd(m, n)
         gcded = gcd(self.numerator, self.denominator)
         self.numerator = self.numerator / gcded
-        self.denominator = self.denominator.gcded
+        self.denominator = self.denominator / gcded
         return self
 
-    def add(self, otherFraction):
-        self.numerator = self.numerator * otherFraction.denominator + otherFraction.numerator * self.denominator
-        self.denominator = self.denominator * otherFraction.denominator
+    def add(self, otherfraction):
 
-    def subtract(self, otherFraction):
-        self.numerator = self.numerator*otherFraction.denominator-otherFraction.numerator*self.denominator
-        self.denominator = self.denominator+otherFraction.denominator
+        self.numerator = (
+            self.numerator * otherfraction.denominator
+            + otherfraction.numerator * self.denominator
+        )
+        self.denominator = self.denominator * otherfraction.denominator
+        self.__reduce()
+        return self
 
-    def multiply(self, otherFraction):
-        self.numerator = self.numerator*otherFraction.numerator
-        self.denominator = self.denominator*otherFraction.denominator
+    def subtract(self, otherfraction):
 
-if __name__ == '__main__':
-    print("** Fraction 1 **")
-    Fraction1 = Fraction(1,2)
+        self.numerator = (
+            (self.numerator * otherfraction.denominator)
+            - (otherfraction.numerator * self.denominator)
+        )
+        self.denominator = self.denominator * otherfraction.denominator
+        self.__reduce()
+        return self
+
+    def multiply(self, otherfraction):
+
+        self.numerator = self.numerator * otherfraction.numerator
+        self.denominator = self.denominator * otherfraction.denominator
+        self.__reduce()
+        return self
+
+
+if __name__ == "__main__":
 
     pass
